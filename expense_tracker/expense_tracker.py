22f5f383745caed6506aad620448d8a4c0f269a6
Aamir <aamir.shaikh.it@gmail.com>
2026-08-05 21:48:45 +0530

    Update expense tracker with full menu functionality

--- expense_tracker/expense_tracker.py
+++ expense_tracker/expense_tracker.py
@@ -1,12 +1,38 @@
-with open("test.txt", "w") as f:
-    f.write("Expence 1\n")
-    f.write("Expence 2\n")
-    f.write("Expence 3\n")
+while True:
 
+    userinput = input("Add / View / Total / Quit: ").lower() 
 
-with open("test.txt", "r") as f:
-    content = f.readlines()
+    if userinput == "add":
+        amountinput = input("Enter the amount: ")
+        categoryinput = input("Enter the category: ")
+        descriptioninput = input("Enter a description: ")
+        with open("expense.txt" , "a") as f:
+            f.write(f"{amountinput},{categoryinput},{descriptioninput}\n")
+        print("Expense added successfully")
+    elif userinput == "view":
+        try:
+            with open("expense.txt", "r") as f:
+                expense = f.readlines()
+            for i in expense:
+                print(i.strip())
+        except FileNotFoundError:
+            print("There Is No Expense Added Yet!")
+    elif userinput == "total":
+        try:
+            with open("expense.txt", "r") as f:
+                expense = f.readlines()
 
+            total = 0
+            for i in expense:
+                parts = i.split(",")
+                amount = parts[0]
+                total += int(amount)
+            print(total)
+        except FileNotFoundError:
+            print("No Expense Recorded Yet. Your Total: 0")
 
-for i in content:
-    print(i.strip())
+    elif userinput == "quit" or userinput == "q" :
+        break
+
+    else:
+        print("Invalid input. Please Choose Form Add / View / Total / Quit.")
